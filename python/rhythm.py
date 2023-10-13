b292f70734a0e4d9ebff468fbcb322f367fa6984
Lars S <>
2023-10-13 14:11:35 -0700

    v0.3.0, I studied permutations a lot yesterday and finally found the fastest algorithm for calculating strum patterns. Read python/rhythm.py for a breakdown of the new algo.

--- python/rhythm.py
+++ python/rhythm.py
@@ -2,43 +2,54 @@
 
 import json 
 
-from itertools import permutations
+# This can calculate size=20 in 0m0.103s where as heap_perm would take hours to calculate size=18
 
+# The premise is that I want every permutation before a certain length
+# Including duplicate digits. The way to achieve duplicate digits with permutations
+# requires input that looks like this [2, 2, 2, 2, 3, 3, 4, 4] and it can only have
+# as many copies of any digit as are in your input, and the max length is huge.
 
-def eighth_note_pool(size: int=8, upper: int=4):
-    pool = []
-    for i in range(2, upper+1):
-        pool += size // i * [i]
-    return pool
+# This is a huge waste because we have to count all the permutations for length of the input.
+# Where as I only care about the length where the sum of all twos is less than or equal
+# to the size. By only using each digit once we have dramatically reduced the amount of work 
+# the computer does by two different factors. Both the max length of the
+# input can be much smaller and the length of desired output is much smaller.
+# Also we are calculating all the different length perms in one go, instead of x different
+# times. I took O(N! * length) down to basically O(N)
 
-# This is the actual function I want, so I need permutations not combos.
-def all_unique_perms(array, size=8, upper=4):
-    result = []
-    for i in range(1, upper+1):
-        for x in permutations(array, i):
-            if sum(x) == size:
-                result.append(tuple(x))
-            
-    return set(tuple(result))
+# This could be recursive but I'm not going to rewrite it.
+def rhythm_permutations(start=[2, 3, 4], size=8):
+    upper = size // 2
+    digits = [[n] for n in start]
+    def worker():
+        for length in range(1, upper):
+            for digit in start:
+                for tail in digits:
+                    if len(tail) == length:
+                        digits.append([digit] + tail)
+
+    worker()
+
+    # This could be moved somewhere else if the user wants my perms for some other use.
+    return [z for z in digits if sum(z) == size]
+
 
 
 # Go back to single json file, and max
 # Load json file and if a key exists then don't render it
 # Then write to a new file so we don't lose the old.
 def write_strums_to_json(max=12, min=4):
-    with open('../json/strumPatterns.json', 'r') as tmp:
-        old = json.load(tmp)
+    #with open('../json/strumPatterns.json', 'r') as tmp:
+    #    old = json.load(tmp)
 
     all_eighth_notes={}
     for x in range(min, max +1):
-        if str(x) not in old:
-            print(f'Finding strum patterns with {x} eighth notes.')
-            pool = eighth_note_pool(size=x, upper=4)
-            perms = list(all_unique_perms(pool, size=x, upper=x//2 +1))
-            all_eighth_notes[x] = sorted(perms)
-    
-            with open(f'../json/newStrumPatterns{x}.json', 'w', encoding='utf-8') as f:
-                json.dump(all_eighth_notes[x], f)
+        print(f'Finding strum patterns with {x} eighth notes.')
+        all_eighth_notes[x] = sorted(rhythm_permutations(size=x))
+        #print(all_eighth_notes)
+    with open(f'../json/newStrumPatterns.json', 'w', encoding='utf-8') as f:
+        json.dump(all_eighth_notes, f)
 
-write_strums_to_json(max=20)
+write_strums_to_json(max=17)
+
 
