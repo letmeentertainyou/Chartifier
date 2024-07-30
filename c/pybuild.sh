6124c6c48b4b88155c2aadd60730f82655e58e46
Lars S <>
2024-07-30 01:20:06 -0700

    v0.9.9 Retooled pybuild so that it doesn't include the full text of setup.py and rhythm_utils.h.

--- c/pybuild.sh
+++ c/pybuild.sh
@@ -8,89 +8,42 @@
 # Change this path to wherever the .so file is generated on your OS and Python version.
 build_path="build/lib.linux-x86_64-cpython-38/rhythm_utils.cpython-38-x86_64-linux-gnu.so"
 
+# The three array libraries you can build with.
+modes=("int" "char" "packed")
 
+# Removes the build artifacts.
 clean() {
     echo "Removing build artifacts."
     rm *.out > /dev/null 2>&1
     rm *.o > /dev/null 2>&1
     rm *.so > /dev/null 2>&1
+    rm setup.py > /dev/null 2>&1
+    rm rhythm_utils.h > /dev/null 2>&1
     rm -r build > /dev/null 2>&1
+}
+
+# Replaces ARRAY_DIR in the template with the requested location.
+# $1 = the array dir, $2 = name of file to update, $3 = extension of file to update.
+update_template() {
+    echo "Writing new $2$3 file."
+    sed "s/ARRAY_DIR/${1}_array/g" templates/$2 > $2$3
 }
 
 # This would be changed if there were a second arg for install mode.
 if [ $# -ne 1 ]; then
     echo "This script takes exactly one arg."
     exit 1
-fi
-
-# When I add a flag for optimization modes I can use a bash variable and plug it into extra_compile_args.
-case "$1" in 
-    "int")
-        array_dir="int_array"
-    ;;
-    "char")
-        array_dir="char_array"
-    ;;
-    "packed")
-        array_dir="packed_char_array"
-    ;;
-    "clean")
-        clean
-        exit 1
-    ;;
-    *)
+elif [ $1 == "clean" ]; then
+    clean
+    exit 1
+elif [[ " ${modes[@]} " =~ " $1 " ]]; then
+    clean  # If I don't clean here then it doesn't compile the code again.
+    update_template $1 "rhythm_utils" ".h"
+    update_template $1 "setup" ".py"
+    python3 setup.py build
+    cp $build_path rhythm_utils.so
+    ./test.py
+else
     echo "The first arg ($1) was not a valid mode."
     exit 1
-    ;;
-esac
-
-# If I don't clean here then it doesn't compile the code again even though the setup.py file has
-# changed, I consider this to be a flaw because I never told it to use the cached build dir.
-clean
-
-# The creates the correct rhythm_utils.h file based on which array library you asked for.
-echo "Removing old rhythm_utils.h file"
-rm rhythm_utils.h > /dev/null 2>&1
-echo "Writing new rhythm_utils.h file."
-
-cat << EOF > rhythm_utils.h
-#ifndef RHYTHM_UTILS_H_
-#define RHYTHM_UTILS_H_
-#include "$array_dir/array.h"
-Array *rhythm_permutations(int size);
-#endif
-EOF
-
-# The creates the correct setup.py file based on which array library you asked for.
-echo removing old setup.py file
-rm setup.py > /dev/null 2>&1
-echo "Writing new setup.py file."
-
-cat << EOF > setup.py
-from setuptools import Extension
-from setuptools import setup
-
-module = Extension("rhythm_utils", sources=["$array_dir/array.c", "rhythm_utils.c", "rhythm_utils_module.c"], extra_compile_args=["-Wall"])
-
-setup(
-    name="rhythm_utils",
-    version="1.0.0",
-    description="An example Python C extension module",
-    url="https://github.com/letmeentertainyou/Chartifier",
-    author="Lars S",
-    author_email="",
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-    ],
-    ext_modules=[module],
-    py_modules=["rhythm"],
-)
-EOF
-
-# Runs the 
-python3 setup.py build
-cp $build_path rhythm_utils.so
-
-# This is optional but it saves me the hassle of typing it out.
-./test.py
+fi
